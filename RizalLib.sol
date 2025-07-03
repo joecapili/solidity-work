23ff9bb7a07ff0766940ca16a6d556965bcfca19
joe <joecapili320@gmail.com>
2025-07-03 13:51:31 +0800

    Edit1RizalLib.sol
    I noticed the libraarian could add itself as a student, so I added a requirement in the addStudent function to stop this from happening

--- RizalLib.sol
+++ RizalLib.sol
@@ -102,7 +102,8 @@ contract RizalLib {
      * @param _student The address of the student to add
      */ 
     function addStudent(address _student) public isLibrarian {
-        
+        require(_student != librarian, "Librarian cannot be a student.");
+
         // This is done in the code for ease of checking 
         require (
             students[_student].idNumber == 0, 
